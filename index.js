6088dadc75cdb2f59a016f302061f4e620069bd3
wagnerona <wagnerolarco@gmail.com>
2023-03-07 11:18:09 +0000

    All prompts work and all data can be stored in the Teams array

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
@@ -18,7 +18,7 @@ const arrayTeam = [];
 
 // prompt for Employee
 
-const teamManager = () => {
+const setManager = () => {
     console.log("Please answer the questions below to build your team's profile");
 
     inquirer.prompt([
@@ -77,7 +77,8 @@ const addMore = () => {
                 break;
 
             default:
-                createHtml();
+                // createHtml();
+                break;
         }
     })
 };
@@ -145,7 +146,7 @@ const addIntern = () => {
 
         // Object Destructing
         const { name, id, email, school } = internAnswers;
-        const intern = new intern(name, id, email, school);
+        const intern = new Intern(name, id, email, school);
 
         arrayTeam.push(intern);
         console.log(intern);
@@ -154,10 +155,7 @@ const addIntern = () => {
 };
 
 
-const createHtml = () => {
-}
 
 
 
-
-teamManager();
+setManager();
